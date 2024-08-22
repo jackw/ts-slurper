f531a24eaef761b1df64b39a74b3dc04481fbfe6
Jack Westbrook <jack.westbrook@gmail.com>
2024-08-22 14:57:46 +0200

    feat: making progress - copy types, generate alias package.json

--- scripts/generatePackage.ts
+++ scripts/generatePackage.ts
@@ -1,117 +1,76 @@
-import { access, constants, mkdir, readdir, writeFile } from "node:fs/promises";
-import { Readable } from "node:stream";
-import { pipeline } from "node:stream/promises";
-import { ReadableStream } from "node:stream/web";
-import { extract } from "tar";
-import { EntryPointConfig, generateDtsBundle } from "dts-bundle-generator";
-import pluginMeta from "../pluginMetadata.json" assert { type: "json" };
-import { join, resolve } from "node:path";
+import { type Dirent } from "node:fs";
+import { mkdir, readdir, writeFile, cp } from "node:fs/promises";
+import { join } from "node:path";
+import PackageJson from "@npmcli/package-json";
 
-const downloadAndExtractRepo = async (
-  repo: string,
-  exportPath: string,
-  pathFilter: string
-) => {
-  await pipeline(
-    await downloadTarStream(`https://codeload.github.com/${repo}/tar.gz/main`),
-    extract({
-      cwd: exportPath,
-      strip: pathFilter.split("/").length,
-      filter: (path) => path.startsWith(pathFilter),
-    })
+async function generateBarrelFile(typesDirs: Dirent[]) {
+  const barrelFile = join("generatedPackage", "dist", "index.ts");
+  const barrelFileContent = typesDirs.map(
+    (dirent) =>
+      `export * as ${kebabToCamel(dirent.name)} from "./${
+        dirent.name
+      }/index.d";`
   );
-};
-
-const downloadTarStream = async (url: string) => {
-  console.log("Downloading github tar...", url);
-  const res = await fetch(url);
-  if (!res.body) {
-    throw new Error(`Failed to download: ${url}`);
-  }
-
-  return Readable.fromWeb(res.body as ReadableStream);
-};
-
-const generateBarrelFile = async () => {
-  const barrelFile = join("src", "index.ts");
-  const barrelFileContent = (await readdir("src", { withFileTypes: true }))
-    .filter((dirent) => dirent.isDirectory())
-    .map(
-      (dirent) =>
-        `export * as ${kebabToCamel(dirent.name)} from "./${dirent.name}";`
-    );
   await writeFile(barrelFile, barrelFileContent.join("\n"));
-};
+}
 
-const generatePackage = async () => {
-  const startTime = Date.now().valueOf();
-  console.log("⚡️ Starting");
-  for (const plugin of pluginMeta) {
+async function createAliasPackageJsonFiles(typesDirs: Dirent[]) {
+  for (const dir of typesDirs) {
     try {
-      const { pluginId, repo, types } = plugin;
-      const exportPath = `./src/${pluginId}`;
-      const pathExists = await directoryExists(exportPath);
-      if (!pathExists) {
-        await mkdir(exportPath, { recursive: true });
-      }
-      const tarName = `${repo.split("/")[1]}-main`;
-      const pathFilter = join(tarName, types);
-
-      await downloadAndExtractRepo(repo, exportPath, pathFilter);
-    } catch (err) {
-      throw new Error(`Failed to generate package: ${err}`);
+      console.log(`📦 Writing package.json for ${dir.name}`);
+      const pkgJsonPath = join("generatedPackage", dir.name);
+      await mkdir(pkgJsonPath, { recursive: true });
+      const pkgJson = await PackageJson.create(pkgJsonPath, {
+        data: {
+          name: `@grafana/${dir.name}`,
+          types: `../dist/${dir.name}/index.d.ts`,
+        },
+      });
+      await pkgJson.save();
+    } catch (error) {
+      console.error(`Error generating package.json for ${dir.name}`, error);
     }
   }
-  await generateBarrelFile();
-  await mkdir("dist", { recursive: true });
-  await generateTypes();
+}
+
+async function createPackageJsonFile(typesDirs: Dirent[]) {
+  const pkgJson = await PackageJson.load("./");
+  pkgJson.update({
+    dependecies: {},
+  });
+  pkgJson.path = join("generatedPackage", "package.json");
+  pkgJson.save();
+  console.log(pkgJson.content);
+}
+
+async function generatePackage(typesDirs: Dirent[]) {
+  const startTime = Date.now().valueOf();
+  console.log("⚡️ Starting");
+  await mkdir(join("generatedPackage", "dist"), { recursive: true });
+  await generateBarrelFile(typesDirs);
+  await copyTypesFiles();
+  await createAliasPackageJsonFiles(typesDirs);
+  await createPackageJsonFile(typesDirs);
 
   const endTime = Date.now().valueOf();
   console.log(`🎉 Done (${endTime - startTime}ms)`);
-};
+}
 
-function kebabToCamel(str) {
+function kebabToCamel(str: string) {
   return str.replace(/-./g, (m) => m.toUpperCase()[1]);
 }
 
-const generateTypes = async () => {
-  const entryPoints: EntryPointConfig[] = [
-    {
-      filePath: "src/index.ts",
-      libraries: {
-        inlinedLibraries: ["@grafana/ui"],
-      },
-      output: {
-        exportReferencedTypes: false,
-      },
-    },
-  ];
-
-  const options = {
-    preferredConfigPath: resolve(__dirname, "../tsconfig.json"),
-  };
-
-  const dts = generateDtsBundle(entryPoints, options);
-  const cleanedDts = cleanDTS(dts);
-  await writeFile("dist/index.d.ts", cleanedDts);
-};
-
-function cleanDTS(dtsContent: string[]) {
-  let dtsString = dtsContent.join("\n");
-  dtsString = dtsString.replace("export {};", "");
-  return dtsString.trim() + "\n";
+async function copyTypesFiles() {
+  const source = join(process.cwd(), "types");
+  const destination = join(process.cwd(), "generatedPackage", "dist");
+  console.log(`📁 Copying ${source} to ${destination}`);
+  await cp(source, destination, { recursive: true });
 }
 
-const directoryExists = async (path: string) => {
-  try {
-    await access(path, constants.F_OK);
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
-
 (async () => {
-  await generatePackage();
+  const typesDirs = (await readdir("types", { withFileTypes: true })).filter(
+    (dirent) => dirent.isDirectory()
+  );
+  await generatePackage(typesDirs);
   process.exit(0);
 })();
